391327f0289abee4cdad9d358ecb06467f7a7f1b
supriyo <hisupriyooo@gmail.com>
2020-08-23 12:27:55 +0530

    Updates

---
+++ precache-manifest.e13f80f4e08be6676da88b7e1935c7c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d8b63bd5c327a6cd801e5cc103d5602",
+    "url": "/devconnector_client/index.html"
+  },
+  {
+    "revision": "8438cf3d027c6507e1c9",
+    "url": "/devconnector_client/static/css/main.662fed2f.chunk.css"
+  },
+  {
+    "revision": "f194481caed2c1d8568f",
+    "url": "/devconnector_client/static/js/2.a669580b.chunk.js"
+  },
+  {
+    "revision": "8438cf3d027c6507e1c9",
+    "url": "/devconnector_client/static/js/main.0fd10ebc.chunk.js"
+  },
+  {
+    "revision": "807ddf4a0d99505c6e42",
+    "url": "/devconnector_client/static/js/runtime~main.0786e01e.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/devconnector_client/static/media/showcase.4b31330b.jpg"
+  }
+]);
